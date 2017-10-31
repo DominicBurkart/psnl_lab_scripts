7083cb71ca5054fdebaea8fabe81145273b93bfa
Dominic Burkart <1dominicb@gmail.com>
2017-10-31 18:18:10 -0400

    matchedness confirmation

--- energy/rdd.R
+++ energy/rdd.R
@@ -1,13 +1,11 @@
+library(sqldf)
+library(lme4)
+
 df = read.csv("/Users/dominicburkart/Documents/princeton/energy project/energy_analysis/summer_data.csv")
 
-#david this is absolutely absurd. i feel like a neanderthal. a cromagnon. a denisovan.
-library(sqldf)
-
 condnames <- unique(df$Condition)
-            
-
 x = aov(avg.power.use ~ Condition, data=df)
-summary(x)
+summary(x) # F of 2.754 P of 0.0275 due to outliers.
 
 l <- vector("list", length(condnames))
 i = 1
@@ -32,4 +30,44 @@ for (c in unique(df$Condition)){
   i = i + 1
 }
 barplot(unlist(l))
+
+m <- vector("list", length(condnames))
+s <- vector("list", length(condnames))
+i = 1
+for (c in unique(df$Condition)){
+  m[[i]] = sqldf(paste('  select avg("avg.power.use") from df where Condition=', "'", c, "' ", sep=""))
+  s[[i]] = sqldf(paste('  select stdev("avg.power.use") from df where Condition=', "'", c, "' ", sep=""))
+  i = i + 1
+}
+segments(barplot(unlist(m)), unlist(m) - unlist(s), barplot(unlist(m)), unlist(m) + unlist(s))
+
 print(condnames)
+
+
+df2 = sqldf(' select * from df order by "avg.power.use" desc')
+#see the first three vals are clear outliers (teterboro)
+df2 = df2[4:nrow(df2),]
+
+summary(aov(scale(avg.power.use) ~ Condition, data=df2)) #F of 0.386 P of 0.819 (even with unaccounted-for colinearity with municipality)
+summary(aov(scale(avg.power.use) ~ scale(cdd) * Condition, data=df2)) # F of 0.982 P of 0.417 (even with unaccounted-for colinearity with municipality)
+summary(lmer(scale(avg.power.use) ~ scale(cdd) + Condition + (1 | MunicipalCityCode), data=df2)) # See anova (following line)
+anova(lmer(scale(avg.power.use) ~ scale(cdd) + Condition + (1 | MunicipalCityCode), data=df2)) # F of 0.3695
+
+summary(lm(avg.power.use ~ Condition, data=df2)) #smallest P: 0.250
+summary(lm(avg.power.use ~ MunicipalCityCode + Condition, data=df2)) #Municipality / Treatments are too colinear without collapsing by town
+summary(lm(avg.power.use ~ MunicipalCityCode + cdd + Condition, data=df2)) #Too colinear
+
+df3 = sqldf(' select avg("avg.power.use"),avg("cdd"),"Condition","MunicipalCityCode" from df2 group by "MunicipalCityCode" ')
+colnames(df3) = c("power", "cdd", "condition", "city")
+summary(aov(power ~ condition, data = df3)) #F of 0.285 P of 0.887
+
+df3 = sqldf(' select median("avg.power.use"),avg("cdd"),"Condition","MunicipalCityCode" from df2 group by "MunicipalCityCode" ')
+colnames(df3) = c("power", "cdd", "condition", "city")
+summary(aov(power ~ condition, data = df3)) #F of 0.181 P of 0.948
+
+df3 = sqldf(' select sum("avg.power.use"),sum("cdd"),"Condition","MunicipalCityCode" from df2 group by "MunicipalCityCode" ')
+colnames(df3) = c("power", "cdd", "condition", "city")
+summary(aov(power ~ condition, data = df3)) #F of 0.285 P of 0.887
+
+
+
